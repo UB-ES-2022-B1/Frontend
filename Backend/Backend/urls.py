0d737ec81e06c462808381abffb4caa7aaf62ef1
Endimbeer Enrique Nuñez Matos <enunezma82@alumnes.ub.edu>
2022-10-09 21:48:06 +0200

    Creación de los primeros test unitarios.

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import re_path
 from clients import views
+
 urlpatterns = [
 
     #Ruta de administración de Django, quitar en producción
@@ -28,5 +29,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^api/clients/$', views.clients_list),
     re_path(r'^api/clients/([0-9])$', views.clients_detail),
-    re_path(r'^api/client/(?P<mail>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$',views.client_detail,name="mail"),
+    re_path(r'^api/client/(?P<mail>[\w.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})/$', views.client_detail, name="mail"),
 ]
---
+++ Backend/tests/ClientsTest.py
@@ -0,0 +1,33 @@
+from django.urls import reverse,path,include
+from rest_framework import status
+from rest_framework.test import APITestCase,URLPatternsTestCase
+from django.test import Client as cl
+from clients.models import Client
+from clients.views import clients_list,client_detail
+
+class ClientTests(APITestCase):
+
+    def test_create_account(self):
+        """
+        Ensure we can create a new client object.
+        """
+        url = reverse(clients_list)
+        data = {'name': 'Lucas',
+        'surname': 'Garcia',
+        'email': 'lgarcia@yahoo.com',
+        'phone': '123091243',
+        'country': 'Argentina',
+        'birthdate': '1987-06-12'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Client.objects.count(), 2)
+        self.assertEqual(Client.objects.get(name='Lucas').name, 'Lucas')
+
+    def test_get_account_by_email(self):
+        """
+        Ensure we can create a new client object.
+        """
+
+        client = cl()
+        response = client.get('http://localhost:8000/api/client/joe@email.com')
+        print(response.status_code)# == 200
