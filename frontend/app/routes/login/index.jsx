a4bf9747f4e0720bc4e1ebe4cb16feeafa657ee3
Xavi Tarres <xtarrero7@alumnes.ub.edu>
2022-10-16 06:37:39 +0200

    login beautified and added extended error management

--- frontend/app/routes/login/index.jsx
+++ frontend/app/routes/login/index.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "@remix-run/react";
-import { useState, useMemo, useEffect } from "react";
+import { useState, useMemo, useEffect, useCallback } from "react";
+import useEffectWithoutFirstRun from '~/utils/useEffectWithoutFirstRun'
 
 import { Input, Button, InputGroup, InputLeftElement,InputRightElement } from '@chakra-ui/react'
 import {
@@ -9,6 +10,18 @@ import {
   FormHelperText,
 } from '@chakra-ui/react'
 
+import {
+  Flex,
+  Box,
+  Heading,
+} from '@chakra-ui/react';
+
+import ErrorMessage from '~/components/ErrorMessage'
+
+const validate = (value) => {
+  return value != ''
+}
+
 
 export default function Index() {
   const [show, setShow] = useState(false)
@@ -17,30 +30,22 @@ export default function Index() {
   const [emailError, setEmailError] = useState(false)
   const [password, setPassword] = useState('');
   const [passwordError, setPasswordError] = useState(false)
-  const [errorMessages, setErrorMessages] = useState({});
+  const [errorMessages, setErrorMessages] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
 
-  //Validate 
-  function performValidation() {
-    return username.length > 0 && password.length > 0;
-    }
-
-  //Displays elements in 
-  const renderErrorMessage = (name) =>{
-    if(name === errorMessages.name)
-      return <div className="error">{errorMessages.message}</div>
-    return null
-    };
 
   async function handleSubmit(event){
-    // Prevent page reload
     event.preventDefault();
+    setErrorMessages('')
+    if(!emailError && !passwordError)
+    {
+    setIsSubmitting(true)
     console.log('Submitted')
     let jsonData={"email":email,"password":password}
     console.log(JSON.stringify(jsonData))
-    let response = await fetch('http://localhost:8000/api/clients/login/',
+    let response = fetch('http://localhost:8000/api/clients/login/',
           {
             method:'POST',
             mode:'cors',
@@ -49,50 +54,101 @@ export default function Index() {
               'Content-Type': 'application/json',
             }
           })
-    console.log(response.json())
+          .then(response => response.json())
+          .then(() =>{
+            setIsSubmitting(false)
+            console.log('response correct? ', resp)
+            })
+          .catch((error)=>{
+            console.log('error detected')
+            setIsSubmitting(false)
+            setErrorMessages(error.error)
+          })
+    }
+    else
+    {
+      setErrorMessages("Please enter valid parameters")
+      setIsSubmitting(false)
+    }
   };
 
-  useEffect(()=>setEmailError(email===''),[email])
-  useEffect(()=>setPasswordError(password===''),[password])
+  //Validations
+  const validateEmail = useCallback(()=>{
+    console.log('validate email')
+    setEmailError(email === '')
+  },[email])
+  const validatePassword = useCallback(()=>{
+    setPasswordError(password === '')
+  },[password])
+  const validateParameters = useCallback(()=>{
+    validateEmail()
+    validatePassword()
+  },[validateEmail,validatePassword])
+
+  const updateEmailError = useEffectWithoutFirstRun(validateEmail,[email])
+  const updatePasswordError = useEffectWithoutFirstRun(validatePassword,[password])
 
 
   return (
-    <div className="login-form" width="500px" center='align'>
+    <div className="login-form">
+      <Flex width="full" align="center" justifyContent="center" padding={"20px"}>
+        <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
+          <Box textAlign="center">
+            <Heading>Login</Heading>
+          </Box>
+          <Box my={4} textAlign="left">
 
-    {isLoggedIn ? <div>Logged in</div> :
-      <form onSubmit={handleSubmit}>
-        <FormControl isInvalid={emailError}>
-          <FormLabel>Email</FormLabel>
-          <Input type='email' value={email} onChange={(e)=>setEmail(e.target.value)} />
-          {!emailError ? null : (
-            <FormErrorMessage>Email is required.</FormErrorMessage>
-          )}
-        </FormControl>
-        <FormControl isInvalid={emailError}>
-          <FormLabel>Password</FormLabel>
-            <InputGroup>
-              <Input
-                type={show ? 'text' : 'password'}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)
-                } />
-              <InputRightElement width='4.5rem'>
-                <Button h='1.75rem' size='sm' onClick={handleClick}>
-                  {show ? 'Hide' : 'Show'}
+            {isLoggedIn ?
+              <Box textAlign="center">
+                <Text>{email} logged in!</Text>
+                <Button
+                  variantColor="orange"
+                  variant="outline"
+                  width="full"
+                  mt={4}
+                  onClick={() => setIsLoggedIn(false)}
+                >
+                  Sign out
                 </Button>
-              </InputRightElement>
-            </InputGroup>
-          {!passwordError ? null : (
-            <FormErrorMessage>Password is required.</FormErrorMessage>
-          )}
-        </FormControl>
-        <Button mt={4} colorScheme='teal' isLoading={isSubmitting} type='submit'>
-          Submit
-        </Button>
-      </form>
-    }
-
-
-    </div>
+              </Box>
+              :
+              <form onSubmit={handleSubmit}>
+                <FormControl isInvalid={emailError}>
+                  <FormLabel>Email</FormLabel>
+                  <Input type='email' value={email} onChange={(e) => setEmail(e.target.value)} />
+                  {!emailError ? null : (
+                    <FormErrorMessage>Email is required.</FormErrorMessage>
+                  )}
+                </FormControl>
+                <FormControl isInvalid={passwordError}>
+                  <FormLabel>Password</FormLabel>
+                  <InputGroup>
+                    <Input
+                      type={show ? 'text' : 'password'}
+                      value={password}
+                      onChange={(e) => { setPassword(e.target.value) }
+                      } />
+                    <InputRightElement width='4.5rem'>
+                      <Button h='1.75rem' size='sm' onClick={handleClick}>
+                        {show ? 'Hide' : 'Show'}
+                      </Button>
+                    </InputRightElement>
+                  </InputGroup>
+                  {!passwordError ? null : (
+                    <FormErrorMessage>Password is required.</FormErrorMessage>
+                  )}
+                </FormControl>
+                {errorMessages && <ErrorMessage message={errorMessages} />}
+                <Box textAlign="center">
+                  <Button mt={4} colorScheme='teal' isLoading={isSubmitting} onClick={validateParameters} type='submit' isDisabled={emailError || passwordError} >
+                    Submit
+                  </Button>
+                </Box>
+              </form>
+            }
+          </Box>
+        </Box>
+      </Flex>
+    </div >
   );
 }
