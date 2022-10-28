4e0a033113d153ae9f025b16ed140e34853c6320
tuduri11 <72009280+tuduri11@users.noreply.github.com>
2022-10-28 15:11:51 +0200

    labels de profile

--- frontend/app/routes/profile/index.jsx
+++ frontend/app/routes/profile/index.jsx
@@ -1,10 +1,44 @@
-
+import {
+    FormControl,
+    FormLabel,
+    FormErrorMessage,
+    FormHelperText,
+    Select,
+    Flex,
+    Box,
+    Heading,
+    Input, 
+    Button, 
+    InputGroup, 
+    InputLeftElement, 
+    InputRightElement 
+  } from '@chakra-ui/react'
 
 export default function Index() {
     
 
     return (
-        console.log('a')
-        
+    <div className="register-form">
+        <Flex width="full" align="center" justifyContent="center" padding={"80px"}>
+            <Box p={8} maxWidth="500px" borderWidth={1} borderRadius={8} boxShadow="lg">
+            <Box textAlign="center">
+                <Heading>{'YOUR PROFILE'}</Heading>
+            </Box>
+                <Box my={4} textAlign="left">
+                    <FormLabel>Name</FormLabel>
+
+                    <FormLabel>Surname</FormLabel>
+                    
+                    <FormLabel>Telephone</FormLabel>
+
+                    <FormLabel>Birth date</FormLabel>
+
+                    <FormLabel>Email</FormLabel>
+
+                    <FormLabel>Password</FormLabel>
+                </Box>
+            </Box>
+        </Flex>
+    </div>
     );
 }
