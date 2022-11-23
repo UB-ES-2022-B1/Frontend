574b1bff26f80a6e4a94280ea00ccb7a539c785b
Joanorteu99 <jorteusa40@alumnes.ub.edu>
2022-11-23 20:16:13 +0100

    Afegir localització, sense haver-ho executat, cal revisar visualització

--- app/components/Location.jsx
+++ app/components/Location.jsx
@@ -1,47 +1,84 @@
 import {
-    Button,
-    FormControl,
-    Flex,
-    Heading,
-    Input,
-    Stack,
-    Text,
-    useColorModeValue,
-  } from '@chakra-ui/react';
+  Button,
+  FormControl,
+  Flex,
+  Heading,
+  Input,
+  Stack,
+  Text,
+  Box,
+  useColorModeValue,
+} from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
-  
-  
-  export default function Location(props){
-    const {onChangeValue} = props
-    const [location, setLocation] = useState("")
 
-    useEffect(()=>onChangeValue({location}),[location])
-    return (
-      <Flex
-        align={'center'}
-        justify={'center'}>
-        <Stack
-          spacing={4}
-          w={'full'}
-          maxW={'md'}
-          p={6}
-          my={12}>
-          <Heading lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
-            Location
+
+export default function Location(props) {
+  const { onChangeValue } = props
+  const [provincia, setProvincia] = useState("")
+  const [carrer, setCarrer] = useState("")
+  const [ciutat, setCiutat] = useState("")
+  const [codi_postal, setCodi_postal] = useState("")
+  const [extra, setExtra] = useState("")
+
+  useEffect(() => onChangeValue({ location }), [location])
+  return (
+    <Flex
+      align={'center'}
+      justify={'center'}>
+      <Box>
+        <Box >
+          <Heading marginY='25px' lineHeight={1.1} fontSize={{ base: '2xl', md: '3xl' }}>
+            Where is your accommodation located?
           </Heading>
-          <Text
-            fontSize={{ base: 'sm', sm: 'md' }}
-            >
-          </Text>
-          <FormControl id="location">
-            <Input
-              placeholder="C/ Gran Via de les Corts Catalanes, 585"
-              _placeholder={{ color: 'gray.500' }}
-              type="text"
-              onChange={(e)=>setLocation(e.target.value)}
-            />
-          </FormControl>
-        </Stack>
-      </Flex>
-    );
-  }
+          <Text marginY='25px'>We will only share the address with guests after they have made the reservation.</Text>
+        </Box>
+
+        <Text marginTop='25px'>Province:</Text>
+        <FormControl id="provincia">
+          <Input
+            placeholder="Barcelona"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+            onChange={(e) => setProvincia(e.target.value)}
+          />
+        </FormControl>
+        <Text marginTop='25px'>Street:</Text>
+        <FormControl id="carrer">
+          <Input 
+            placeholder="C/ Gran Via de les Corts Catalanes, 585"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+            onChange={(e) => setCarrer(e.target.value)}
+          />
+        </FormControl>
+        <Text marginTop='25px'>City:</Text>
+        <FormControl id="ciutat">
+          <Input
+            placeholder="Barcelona"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+            onChange={(e) => setCiutat(e.target.value)}
+          />
+        </FormControl>
+        <Text marginTop='25px'>CP::</Text>
+        <FormControl id="codiPostal">
+          <Input
+            placeholder="12345"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+            onChange={(e) => setCodi_postal(e.target.value)}
+          />
+        </FormControl>
+        <Text marginTop='25px'>Estra:</Text>
+        <FormControl id="extra">
+          <Input
+            placeholder="2n,1"
+            _placeholder={{ color: 'gray.500' }}
+            type="text"
+            onChange={(e) => setExtra(e.target.value)}
+          />
+        </FormControl>
+      </Box>
+    </Flex>
+  );
+}
