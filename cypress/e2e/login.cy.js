991cf5cb046c5acc8bb0cbb9b49368eec759883d
ACouceiroM <acoucema7@alumnes.ub.edu>
2022-11-27 19:01:31 +0100

    login cypress testing for all ac

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,23 +1,36 @@
 describe("Login and Logout", () => {
     beforeEach(() => {
     // runs before each test in the block
-        cy.visit("https://test-dev--housh.netlify.app/login/");
+        cy.visit("https://test-dev--housh.netlify.app");
         cy.clearCookies()
+        cy.get("*[class='dropdown_activator']").click()
+        cy.get("*[class='item_list']").children().contains("Log in").click({force:true})
+        cy.url().should('eq', 'https://test-dev--housh.netlify.app/login/')
   })
   it("Can login through the UI", function () {
     cy.get("input[type='email']").type("cypress@cypressauto.com");
     cy.get("input[type='password']").type("CypressTesting$1");
     cy.get("form").submit();
     cy.wait(5000)
-    cy.get("button[type='button']").contains("Sign out").should("exist").click();
+    cy.get("button[type='button']").contains("Home").should("exist").click();
+
+    cy.get("*[class='dropdown_activator']").click()
+    cy.get("*[class='item_list']").children().contains("See profile").click()
+
+    cy.get("[class='chakra-text css-0']").contains('Email address').siblings().should((elem) => {
+      expect(elem.text()).to.equal('cypress@cypressauto.com');
+    });
   });
 });
 
 describe("Login after 4 attempts", () => {
     beforeEach(() => {
     // runs before each test in the block
-        cy.visit("https://test-dev--housh.netlify.app/login/");
+        cy.visit("https://test-dev--housh.netlify.app");
         cy.clearCookies()
+        cy.get("*[class='dropdown_activator']").click()
+        cy.get("*[class='item_list']").children().contains("Log in").click({force:true})
+        cy.url().should('eq', 'https://test-dev--housh.netlify.app/login/')
   })
   it("4 bad attempts", function () {
 
@@ -56,8 +69,11 @@ describe("Login after 4 attempts", () => {
 describe("Login with Bad Name", () => {
     beforeEach(() => {
     // runs before each test in the block
-        cy.visit("https://test-dev--housh.netlify.app/login/");
+        cy.visit("https://test-dev--housh.netlify.app");
         cy.clearCookies()
+        cy.get("*[class='dropdown_activator']").click()
+        cy.get("*[class='item_list']").children().contains("Log in").click({force:true})
+        cy.url().should('eq', 'https://test-dev--housh.netlify.app/login/')
   })
   it("Try to login with bad name", function () {
     cy.get("input[type='email']").type("falso@cypressauto.com");
@@ -65,4 +81,21 @@ describe("Login with Bad Name", () => {
     cy.get("form").submit();
     cy.get("*[role='alert']").should("exist").contains("User not exists")
   });
+});
+
+describe("Login with Blocked User", () => {
+    beforeEach(() => {
+      // runs before each test in the block
+      cy.visit("https://test-dev--housh.netlify.app");
+      cy.clearCookies()
+      cy.get("*[class='dropdown_activator']").click()
+      cy.get("*[class='item_list']").children().contains("Log in").click({force:true})
+      cy.url().should('eq', 'https://test-dev--housh.netlify.app/login/')
+  })
+    it("Try to login with blocked user", function () {
+      cy.get("input[type='email']").type("blocked@blocked.com");
+      cy.get("input[type='password']").type("Blocked$1");
+      cy.get("form").submit();
+      cy.get("*[role='alert']").should("exist").contains("Block user")
+    });
 });
