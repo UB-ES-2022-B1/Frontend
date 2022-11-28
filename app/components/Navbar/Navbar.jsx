36d06804fcbdbf0930fea952706d05c21aa4bbc1
joan <jorteusa40@alumnes.ub.edu>
2022-11-28 17:40:12 +0100

    Casi acabat, falta cambiar colo botons

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -15,18 +15,29 @@ import {
     Spacer,
     IconButton,
     Center,
+    background,
 } from '@chakra-ui/react'
 import { Search2Icon } from '@chakra-ui/icons'
-import { useState } from 'react';
+import {
+    useState,
+    useCallback,
+    useEffect
+} from 'react';
 import Dropdown from "~/components/Dropdown";
+import { useFetcher } from 'react-router-dom';
 
 
 export default function (params) {
+    //declarant variables
     const [dateStart, setDateStart] = useState("")
     const [dateEnd, setDateEnd] = useState("")
     const [people, setPeople] = useState(0)
     const [location, setLocation] = useState("")
-    
+    var locationError = { locationError: false, locationErrorMess: "" };
+    var dateEndError = { dateEndError: false, dateEndErrorMess: "" };
+    var dateStartError = { dateStartError: false, dateStartErrorMess: "" };
+
+    //declarem funcions
     const decrease = () => {
         setPeople(people - 1);
     }
@@ -35,6 +46,53 @@ export default function (params) {
         setPeople(people + 1);
     }
 
+    //validar parametres de input de cerca
+    const validateLocation = useCallback(() => {
+        if (location.match(/^[A-Za-z]+$/) === null) {
+            locationError['locationErrorMess'] = 'Location can\'t contain numbers';
+            locationError['locationError'] = true;
+        } else {
+            locationError['locationError'] = false;
+        }
+        console.log(locationError["locationErrorMess"])
+    }, [location])
+
+    const validateEndDate = useCallback(() => {
+        let end = new Date(dateEnd)
+        let start = new Date(dateStart)
+        if (end>start) {
+            locationError['dateEndErrorMess'] = 'End date must be later than start date';
+            locationError['dateEndError'] = true;
+        } else {
+            locationError['dateEndError'] = false;
+        }
+        console.log(locationError["dateEndErrorMess"])
+    }, [dateEnd,dateStart])
+
+    const validateStartDate = useCallback(() => {
+        let today = new Date(currentDate)
+        let start = new Date(dateStart)
+        if (today >= start) {
+            locationError['dateStartErrorMess'] = 'Start date must be later or equal to today';
+            locationError['dateStartError'] = true;
+        } else {
+            locationError['dateStartError'] = false;
+        }
+        console.log(locationError["dateStartErrorMess"])
+    }, [dateStart])
+    
+    
+
+    const validateParam = useCallback(() => {
+        validateLocation()
+        validateStartDate()
+        validateEndDate()
+    }, [validateLocation,validateEndDate])
+
+    //Crides a back end
+    async function handleSubmit(event) { }
+
+    
     console.log(dateEnd);
     console.log(dateStart);
     console.log(people);
@@ -50,26 +108,28 @@ export default function (params) {
 
                     <Box textAlign="center">
 
-                        <FormControl as='fieldset'>
+                        <FormControl as='fieldset' isInvalid={locationError["locationError"]}>
+                            
+                                <Popover>
+                                    <PopoverTrigger>
+                                        <Button variant='ghost' style = {{color:locationError["locationError"]? "red":"black"}} borderRadius={30}>Destiny</Button>
+                                        
+                                    </PopoverTrigger>
+                                    <Portal>
+                                        <PopoverContent>
+                                            <PopoverArrow />
+                                            <PopoverHeader>Where?</PopoverHeader>
+                                            <PopoverCloseButton />
+                                            <PopoverBody>
+                                                <Input placeholder='Destiny' value = {location} onChange={(e) => setLocation(e.target.value)} />
+                                            </PopoverBody>
+                                        </PopoverContent>
+                                    </Portal>
+                                </Popover>
+                            
                             <Popover>
                                 <PopoverTrigger>
-                                    <Button variant='ghost' borderRadius={30}>Destiny</Button>
-                                </PopoverTrigger>
-                                <Portal>
-                                    <PopoverContent>
-                                        <PopoverArrow />
-                                        <PopoverHeader>Where?</PopoverHeader>
-                                        <PopoverCloseButton />
-                                        <PopoverBody>
-                                            <Input placeholder='Destiny' onChange={(e) => setLocation(e.target.value)} />
-                                        </PopoverBody>
-                                    </PopoverContent>
-                                </Portal>
-                            </Popover>
-
-                            <Popover>
-                                <PopoverTrigger>
-                                    <Button variant='ghost' borderRadius={30}>Arrival</Button>
+                                    <Button variant='ghost' style = {{color:dateStartError["dateStartError"]? "red":"black"}} borderRadius={30}>Arrival</Button>
                                 </PopoverTrigger>
                                 <Portal>
                                     <PopoverContent>
@@ -85,7 +145,7 @@ export default function (params) {
 
                             <Popover>
                                 <PopoverTrigger>
-                                    <Button variant='ghost' borderRadius={30}>Departure</Button>
+                                    <Button variant='ghost' style = {{color:dateEndError["dateEndError"]? "red":"black"}} borderRadius={30}>Departure</Button>
                                 </PopoverTrigger>
                                 <Portal>
                                     <PopoverContent>
@@ -119,7 +179,10 @@ export default function (params) {
                                     </PopoverContent>
                                 </Portal>
                             </Popover>
-                            <IconButton colorScheme='purple' borderRadius={30} aria-label='Search' icon={<Search2Icon />} />
+                            <IconButton colorScheme='purple' borderRadius={30} aria-label='Search' icon={<Search2Icon />}
+                                onClick={validateParam}//handleSubmit
+                                isDisabled={locationError['locationError']}
+                            />
                         </FormControl>
                     </Box>
                 </Center>
