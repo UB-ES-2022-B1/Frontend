396746966184c03dda6a9d73fdf45e73b3b5e965
Pedro <psendoco7@alumnes.ub.edu>
2022-12-06 19:00:27 +0100

    Footer done

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -1,31 +1,37 @@
 import {
     Box,
     Button,
-    ButtonGroup,
     Divider,
     Flex,
+    Spacer,
     Text,
 } from '@chakra-ui/react'
 
-import React, { useState, useCallback } from 'react'
-
 export default function () {
 
+    const navicateToPrivacy = () => {
+        //window.location.href = '/privacy';
+    }
+    const navicateToCondition = () => {
+        //window.location.href = '/condition';
+    }
 
     return (
         <Flex width="100%" height='50px' position='fixed'  left='0' bottom='0'>
             <Box width="full" backgroundColor="#FAF7F0">
                 <Divider></Divider>
-                <Flex marginLeft='20' marginTop='2'>
+                <Flex marginLeft='95' marginTop='2'>
                     <Text color='#696969'>© Housh 2022</Text>
                     <Text color='#696969' marginLeft='3'>·</Text>
-                    <Button variant='link' color='#808080' marginLeft='3'>
+                    <Button variant='link' color='#808080' marginLeft='3' onClick={navicateToPrivacy()}>
                         Privacy
                     </Button>
                     <Text color='#696969' marginLeft='3'>·</Text>
-                    <Button variant='link' color='#808080' marginLeft='3'>
+                    <Button variant='link' color='#808080' marginLeft='3' onClick={navicateToCondition()}>
                         Conditions
                     </Button>
+                    <Spacer></Spacer>
+                    <Text color='#696969' marginRight='95'>€ EUR</Text>
                 </Flex>
             </Box>
         </Flex>
